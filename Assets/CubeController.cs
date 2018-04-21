e71bb9db50b15f599f7914dcfbf57c7dab353376
tomoya.oono <tomoya00060107@gmail.com>
2018-04-21 21:09:14 +0900

    GoForwardUnityChan　初めてのコミット 課題提出

---
+++ Assets/CubeController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "GroundTag")
+        {
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.Play();
+        }
+
+        else if (coll.gameObject.tag == "CubeTag")
+        {
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.Play();
+        }
+    }
+
+    // キューブの移動速度
+    private float speed = -0.2f;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
